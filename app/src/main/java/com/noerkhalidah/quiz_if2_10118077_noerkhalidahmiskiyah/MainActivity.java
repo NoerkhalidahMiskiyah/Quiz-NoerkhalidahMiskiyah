f4631a5b5c83f46c50811b4bb07825451da482f5
Noerkhalidah Miskiyah <nurmiskiyah50@gmail.com>
2021-05-29 10:56:29 +0700

    Initial commit

---
+++ app/src/main/java/com/noerkhalidah/quiz_if2_10118077_noerkhalidahmiskiyah/MainActivity.java
@@ -0,0 +1,14 @@
+package com.noerkhalidah.quiz_if2_10118077_noerkhalidahmiskiyah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
